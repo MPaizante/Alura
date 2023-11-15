3211a6381921f50d191c5c2c3114463a5ec1a127
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-15 18:45:32 -0300

    Update Aula03-01.js

--- Imersão-Dev-2023.2/Aula03/Aula03-01.js
+++ Imersão-Dev-2023.2/Aula03/Aula03-01.js
@@ -1,16 +1,8 @@
-var filmeMatheus = 'https://papofuradopodcast.files.wordpress.com/2022/02/the_batman_poster_10.jpg';
 
-var filmePaizante = 'https://br.web.img3.acsta.net/medias/nmedia/18/87/29/99/19874070.jpg';
-
-var filmeAraujo = 'https://br.web.img2.acsta.net/pictures/14/08/01/19/10/403236.jpg';
-
-var filmeMeu = 'https://musicart.xboxlive.com/7/11601100-0000-0000-0000-000000000002/504/image.jpg';
+var listaFilmes = ['https://papofuradopodcast.files.wordpress.com/2022/02/the_batman_poster_10.jpg', 'https://br.web.img3.acsta.net/medias/nmedia/18/87/29/99/19874070.jpg', 'https://br.web.img2.acsta.net/pictures/14/08/01/19/10/403236.jpg', 'https://musicart.xboxlive.com/7/11601100-0000-0000-0000-000000000002/504/image.jpg'];
 
 
-var listaFilmes = [filmeMatheus, filmePaizante, filmeAraujo, filmeMeu];
-
-
-for(i = 0; i < 4 ; i++){
+for(i = 0; i < listaFilmes.length ; i++){
     document.write('<img src=' + listaFilmes[i] + '>');
 }
 
