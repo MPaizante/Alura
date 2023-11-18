6f072ba52c687d9586f97d1579779de272e85103
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-17 23:33:15 -0300

    Update Aula05-01.js

--- Imersão-Dev-2023.2/Aula05/Aula05-01.js
+++ Imersão-Dev-2023.2/Aula05/Aula05-01.js
@@ -1,5 +1,45 @@
+var paulo = {
+    nome:'Paulo',
+    vitoria: 10,
+    empate:0,
+    derrota:0,
+    pontos:0
+}
+var rafa = {
+    nome:'Rafa',
+    vitoria: 10,
+    empate:0,
+    derrota:0,
+    pontos:0
+}
+var gui = {
+    nome:'Gui',
+    vitoria: 10,
+    empate:0,
+    derrota:0,
+    pontos:0
+}
+var renato = {
+    nome:'Renato',
+    vitoria: 10,
+    empate:0,
+    derrota:0,
+    pontos:0
+}
 
-
+var elementoTabela = document.getElementById('tabelaJogadores')
+elementoTabela.innerHTML = `
+    <tr>
+        <td>Paulo</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><button onClick="adicionarVitoria()">Vitória</button></td>
+        <td><button onClick="adicionarEmpate()">Empate</button></td>
+        <td><button onClick="adicionarDerrota()">Derrota</button></td>
+    </tr>
+`
 
 function adicionarVitoria(){
 
