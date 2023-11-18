1a21c7f57021cd7620b6aa77e183421633e2a401
Matheus Paizante <matheus_paizante@hotmail.com>
2023-11-18 00:04:40 -0300

    .

--- Imersão-Dev-2023.2/Aula05/Aula05-01.js
+++ Imersão-Dev-2023.2/Aula05/Aula05-01.js
@@ -1,52 +1,68 @@
 var paulo = {
-    nome:'Paulo',
+    nome: "Paulo",
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+  };
+  
+  var rafa = {
+    nome: "Rafa",
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+  };
+  
+  var gui = {
+    nome: "Gui",
     vitoria: 10,
-    empate:0,
-    derrota:0,
-    pontos:0
-}
-var rafa = {
-    nome:'Rafa',
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+  };
+  
+  var renato = {
+    nome: "Renato",
     vitoria: 10,
-    empate:0,
-    derrota:0,
-    pontos:0
-}
-var gui = {
-    nome:'Gui',
-    vitoria: 10,
-    empate:0,
-    derrota:0,
-    pontos:0
-}
-var renato = {
-    nome:'Renato',
-    vitoria: 10,
-    empate:0,
-    derrota:0,
-    pontos:0
-}
-
-var elementoTabela = document.getElementById("tabelaJogadores");
-elementoTabela.innerHTML = `
-    <tr>
-        <td>${paulo.nome}</td>
-        <td>${paulo.vitoria}</td>
-        <td>${paulo.empate}</td>
-        <td>${paulo.derrota}</td>
-        <td>${paulo.pontos}</td>
-        <td><button onClick="adicionarVitoria(paulo)">Vitória</button></td>
-        <td><button onClick="adicionarEmpate()">Empate</button></td>
-        <td><button onClick="adicionarDerrota()">Derrota</button></td>
-    </tr>
-`;
-
-function adicionarVitoria(jogador){
-    console.log(jogador)
-}
-function adicionarEmpate(){
-    
-}
-function adicionarDerrota(){
-    
-}
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+  };
+  
+  var elementoTabela = document.getElementById("tabelaJogadores");
+  
+  exibirNaTela();
+  
+  function exibirNaTela() {
+    elementoTabela.innerHTML = `
+        <tr>
+            <td>${paulo.nome}</td>
+            <td>${paulo.vitoria}</td>
+            <td>${paulo.empate}</td>
+            <td>${paulo.derrota}</td>
+            <td>${paulo.pontos}</td>
+            <td><button onClick="adicionarVitoria(paulo)">Vitória</button></td>
+            <td><button onClick="adicionarEmpate(paulo)">Empate</button></td>
+            <td><button onClick="adicionarDerrota(paulo)">Derrota</button></td>
+        </tr>
+    `;
+  }
+  
+  function adicionarVitoria(jogador) {
+    jogador.vitoria++;
+    jogador.pontos = jogador.pontos + 3;
+    exibirNaTela();
+  }
+  
+  function adicionarEmpate(jogador) {
+    jogador.empate++;
+    jogador.pontos++;
+    exibirNaTela();
+  }
+  
+  function adicionarDerrota(jogador) {
+    jogador.derrota++;
+    exibirNaTela();
+  }
+  
