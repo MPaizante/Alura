70e1dc33543b9c2ed47fbd595c0ed0945d72783b
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-11 21:55:37 -0300

    Update App.vue

--- pokevite/src/App.vue
+++ pokevite/src/App.vue
@@ -8,12 +8,23 @@ let num = ref (0);
 <template>
   <!--NAVBAR-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
   <div class="container-fluid">
-    <router-link class="nav-link " aria-current="page" to="/" >NavBar</router-link>
+    <router-link class="navbar-brand" to="/">
+      <img src="https://www.pngall.com/wp-content/uploads/5/Pokemon-Pikachu-PNG-Image-File.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      PokeVite
+    </router-link> 
+
+    <router-link class="nav-link " aria-current="page" to="/" >PokeVite</router-link>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
       <span class="navbar-toggler-icon"></span>
+
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNav">
+
       <ul class="navbar-nav">
         <li class="nav-item">
            <router-link class="nav-link " aria-current="page" to="/" >Home</router-link>
@@ -24,12 +35,34 @@ let num = ref (0);
         </li>
         
       </ul>
+
     </div>
+
   </div>
+
 </nav>
+
 
 <!--CONTENT-->
 <div class="main">
   <router-view></router-view>  
 </div>
+
+<footer class="bg-primary text-light">
+  <div>
+    {{ new Date().getFullYear() }}
+  </div>
+</footer>
 </template>
+
+<style>
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+}
+</style>
