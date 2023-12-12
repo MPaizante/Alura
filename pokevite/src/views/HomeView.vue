1089524b69cc68266841a3e48bd552cb55850b71
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-11 23:25:20 -0300

    Update HomeView.vue

--- pokevite/src/views/HomeView.vue
+++ pokevite/src/views/HomeView.vue
@@ -4,6 +4,36 @@
 
 <template>
   <main>
-    Home
+
+    <div class="container">
+      
+      <div class="row mt-4">
+
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/27/pokemon-ash-pikachu-capa-1hbirpgndicl1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/27/pokemon-ash-pikachu-capa-1hbirpgndicl1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
   </main>
 </template>
