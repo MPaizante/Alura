b65058fc1d06f7ff21168b9af457ea427ead4583
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-12 22:43:30 -0300

    .

--- pokevite/src/App.vue
+++ pokevite/src/App.vue
@@ -1,62 +1,70 @@
 <script setup>
-import { ref } from 'vue';
-let num = ref (0);
-
-
+import { ref } from "vue";
+let num = ref(0);
 </script>
 
 <template>
   <!--NAVBAR-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <img
+          src="https://www.pngall.com/wp-content/uploads/5/Pokemon-Pikachu-PNG-Image-File.png"
+          alt="Logo"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        PokeVite
+      </router-link>
 
-  <div class="container-fluid">
-    <router-link class="navbar-brand" to="/">
-      <img src="https://www.pngall.com/wp-content/uploads/5/Pokemon-Pikachu-PNG-Image-File.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      PokeVite
-    </router-link> 
+      <router-link class="nav-link" aria-current="page" to="/"
+        >PokeVite</router-link
+      >
 
-    <router-link class="nav-link " aria-current="page" to="/" >PokeVite</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-
-      <span class="navbar-toggler-icon"></span>
-
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNav">
-
-      <ul class="navbar-nav">
-        <li class="nav-item">
-           <router-link class="nav-link " aria-current="page" to="/" >Home</router-link>
-           
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link " aria-current="page" to="/about" >About</router-link>
-        </li>
-        
-      </ul>
-
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/about"
+              >About</router-link
+            >
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
 
+  <!--CONTENT-->
+  <div class="main">
+    <router-view></router-view>
   </div>
 
-</nav>
-
-
-<!--CONTENT-->
-<div class="main">
-  <router-view></router-view>  
-</div>
-
-<footer class="bg-primary text-light">
-  <div>
-    {{ new Date().getFullYear() }}
-  </div>
-</footer>
+  <footer class="bg-primary text-light">
+    <div>
+      {{ new Date().getFullYear() }}
+    </div>
+  </footer>
 </template>
 
 <style>
-footer{
+footer {
   position: fixed;
   bottom: 0;
   width: 100%;
--- pokevite/src/views/HomeView.vue
+++ pokevite/src/views/HomeView.vue
@@ -1,39 +1,48 @@
 <script setup>
+import { onMounted, reactive, ref } from "vue";
 
+let pokemons = reactive(ref());
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    .then((res) => res.json())
+    .then((res) => (pokemons.value = res.results));
+});
 </script>
 
 <template>
   <main>
-
     <div class="container">
-      
       <div class="row mt-4">
-
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/27/pokemon-ash-pikachu-capa-1hbirpgndicl1.jpg" class="card-img-top" alt="...">
+          <div class="card" style="width: 18rem">
+            <img
+              src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/27/pokemon-ash-pikachu-capa-1hbirpgndicl1.jpg"
+              class="card-img-top"
+              alt="..."
+            />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
             </div>
           </div>
         </div>
 
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/27/pokemon-ash-pikachu-capa-1hbirpgndicl1.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              
-            </div>
+          <div class="card" style="width: 18rem">
+            <ul>
+              <li v-for="pokemon in pokemons" :key="pokemon.name">
+
+                {{ pokemon.name }}
+                
+              </li>
+            </ul>
           </div>
         </div>
-
       </div>
-
     </div>
-
   </main>
 </template>
